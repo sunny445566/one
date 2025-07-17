fba18a7236e45ebb25e1202f2bb3a7d417129fda
sunny445566 <designer.saga123@gmail.com>
2025-07-17 12:57:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
     document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
+      behavior: 'hard'
     });
   });
 });
